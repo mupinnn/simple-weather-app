cc72eb5d404e6a0df2020db0ac2f306ed774275b
mupinnn <muaffaqfaqih@gmail.com>
2019-09-23 22:51:07 +0700

    create base components and styles for 5 day forecast, and fixing previous styles

--- src/components/NextFiveDayForecasts.js
+++ src/components/NextFiveDayForecasts.js
@@ -5,9 +5,46 @@ class NextFiveDayForecasts extends React.Component {
 
     render() {
         return (
-            <div className="Cards">
-                <div className="Card-next-five"></div>
-            </div>
+            <React.Fragment>
+                <h3 className="Result-info">5 day weather forecast</h3>
+                <div className="Cards-next-five">
+                    <div className="Card-next-five">
+                        <p className="Date-time">Tue, 24 Sept 2019</p>
+                        <p className="Weather-desc">light rain</p>
+                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
+                        <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+                        <p className="Humid">humidity 35%</p>
+                    </div>
+                    <div className="Card-next-five">
+                        <p className="Date-time">Tue, 24 Sept 2019</p>
+                        <p className="Weather-desc">light rain</p>
+                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
+                        <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+                        <p className="Humid">humidity 35%</p>
+                    </div>
+                    <div className="Card-next-five">
+                        <p className="Date-time">Tue, 24 Sept 2019</p>
+                        <p className="Weather-desc">light rain</p>
+                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
+                        <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+                        <p className="Humid">humidity 35%</p>
+                    </div>
+                    <div className="Card-next-five">
+                        <p className="Date-time">Tue, 24 Sept 2019</p>
+                        <p className="Weather-desc">light rain</p>
+                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
+                        <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+                        <p className="Humid">humidity 35%</p>
+                    </div>
+                    <div className="Card-next-five">
+                        <p className="Date-time">Tue, 24 Sept 2019</p>
+                        <p className="Weather-desc">light rain</p>
+                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
+                        <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+                        <p className="Humid">humidity 35%</p>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 
--- src/components/TodayForecasts.js
+++ src/components/TodayForecasts.js
@@ -5,49 +5,51 @@ class TodayForecasts extends React.Component {
 
     render() {
         return (
-            <div className="Cards">
-                <h3 className="Result-title">Current weather based on your location / search :</h3>
-                <div className="Card-today">
-                    <div className="Weather-details">
-                        <p className="Date-time">16:45, 23 September 2019</p>
-                        <table>
-                            <tr>
-                                <td>humidity</td>
-                                <td>52%</td>
-                            </tr>
-                            <tr>
-                                <td>main</td>
-                                <td>clouds</td>
-                            </tr>
-                            <tr>
-                                <td>pressure</td>
-                                <td>1013</td>
-                            </tr>
-                            <tr>
-                                <td>min temp</td>
-                                <td>18 &#x2103; / 54 &#x2109;</td>
-                            </tr>
-                            <tr>
-                                <td>max temp</td>
-                                <td>18 &#x2103; / 54 &#x2109;</td>
-                            </tr>
-                            <tr>
-                                <td>sunrise</td>
-                                <td>18 &#x2103; / 54 &#x2109;</td>
-                            </tr>
-                            <tr>
-                                <td>sunset</td>
-                                <td>18 &#x2103; / 54 &#x2109;</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div className="Weather-cond">
-                        <p className="Cond-desc">Few clouds</p>
-                        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
-                        <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+            <React.Fragment>
+                <h3 className="Result-info">Current weather based on your location / search :</h3>
+                <div className="Cards-today">
+                    <h2 className="City-name">Jakarta, ID</h2>
+                    <div className="Card-today">
+                        <div className="Weather-details">
+                            <p className="Date-time">16:45, 23 Sept 2019</p>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>humidity</td>
+                                        <td>52%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>pressure</td>
+                                        <td>1013</td>
+                                    </tr>
+                                    <tr>
+                                        <td>min temp</td>
+                                        <td>18 &#x2103; / 54 &#x2109;</td>
+                                    </tr>
+                                    <tr>
+                                        <td>max temp</td>
+                                        <td>18 &#x2103; / 54 &#x2109;</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sunrise</td>
+                                        <td>18 &#x2103; / 54 &#x2109;</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sunset</td>
+                                        <td>18 &#x2103; / 54 &#x2109;</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="Weather-cond">
+                            <p className="Weather-main">Clouds,</p>
+                            <p className="Weather-desc">Few clouds</p>
+                            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" />
+                            <p className="Temperature-cond">32 &#x2103; / 89.6 &#x2109;</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 
