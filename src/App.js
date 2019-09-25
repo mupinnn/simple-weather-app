228bf7a834bb00282992d195d00baabd4d353bc2
mupinnn <muaffaqfaqih@gmail.com>
2019-09-25 15:58:44 +0700

    succes to get current position with geolocation, success get data by city name. fixing the functionality still on progress

--- src/App.js
+++ src/App.js
@@ -12,72 +12,90 @@ class App extends React.Component {
             currentWeatherData: [],
             fiveDayForecastData: [],
             searchedCity: "",
-            currentLat: "",
-            currentLong: ""
+            inputVal: ""
         }
         
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
     }
 
-    // Handle input change
-    handleInput = (e) => {
-        this.setState({searchedCity: e.target.value});
-    }
-
-    // Handle form submit
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.setState({searchedCity: this.state.searchedCity});
-    }
-
     // Get data by name
     getWeatherDataByCityName = (name) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${process.env.REACT_APP_API_KEY}`)
             .then(response => response.json())
-            .then(result => console.log(result));
+            .then(result => this.setState({currentWeatherData: result}));
     }
 
     // Get data by latitude & longitude
     getWeatherDataByLatLong = (lat, long) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`)
             .then(response => response.json())
-            .then(result => console.log(result));
+            .then(result => this.setState({currentWeatherData: result}));
+    }
+
+    // Handle input change
+    handleInput = (e) => {
+        this.setState({inputVal: e.target.value});
+    }
+
+    // Handle form submit
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            searchedCity: this.state.inputVal,
+            inputVal: ""
+        });
+
+        this.getWeatherDataByCityName(this.state.searchedCity);
     }
 
     // Show current latitude, longitude position
     showPosition = (position) => {
-        this.setState({
-            currentLat: position.coords.latitude,
-            currentLong: position.coords.longitude
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+
+        this.getWeatherDataByLatLong(lat, long);
+    }
+
+    errPosition = () => {
+        console.log("denied");
+    }
+
+    // Handle geolocation permission
+    handleGeoPermission = () => {
+        const geoSettings = {enableHighAccuracy: true};
+        const showPosition = this.showPosition;
+        const errPosition = this.errPosition;
+        navigator.permissions.query({name: "geolocation"}).then(function(result) {
+            if (result.state === "granted") {
+                console.log(result.state);
+                navigator.geolocation.getCurrentPosition(showPosition, errPosition, geoSettings);
+            } else if (result.state === "prompt") {
+                console.log(result.state);
+                navigator.geolocation.getCurrentPosition(showPosition, errPosition, geoSettings);
+            } else {
+                console.log("denied");
+            }
         });
     }
 
     // Do componentDidMount, and check if geolocation feature is allowed.
     componentDidMount() {
-        const pos = this.showPosition;
         if (navigator.geolocation) {
-            navigator.permissions.query({name: "geolocation"}).then(function(result) {
-                if (result.state === "granted") {
-                    console.log("allowed");
-                } else {
-                    console.log("blocked");
-                }
-            });
+            this.handleGeoPermission();
         } else {
-            console.log("geolocation isn't supported in your browser");
+            console.log("your browser doesn't support geolocation features. pls type on the field above");
         }
     }
-    
 
     render() {
-        const {searchedCity} = this.state;
+        const {inputVal, searchedCity, currentWeatherData, fiveDayForecastData} = this.state;
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Simple Weather Forecasts App</h1>
                 </header>
-                <SearchBar inputValue={searchedCity} searchSubmit={this.handleSubmit} searchInput={this.handleInput} />
+                <SearchBar inputValue={inputVal} searchSubmit={this.handleSubmit} searchInput={this.handleInput} />
                 <TodayForecasts />
                 <NextFiveDayForecasts />
                 <footer className="App-footer">
